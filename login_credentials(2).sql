ced39df214a71b0453d5e05bfeea15b671ce15bd
Kruczek Radosław <73522963+kruczek-radoslaw@users.noreply.github.com>
2026-05-01 14:19:59 +0200

    Add files via upload

--- login_credentials(2).sql
+++ login_credentials(2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: db
--- Generation Time: Apr 23, 2026 at 05:55 PM
+-- Generation Time: Maj 01, 2026 at 12:18 PM
 -- Wersja serwera: 9.6.0
 -- Wersja PHP: 8.3.30
 
@@ -47,6 +47,7 @@ INSERT INTO `permissions` (`permission_id`, `name`) VALUES
 --
 
 CREATE TABLE `users` (
+  `user_id` int NOT NULL,
   `login` varchar(30) COLLATE utf8mb4_general_ci NOT NULL,
   `password` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `permission` int NOT NULL
@@ -56,9 +57,16 @@ CREATE TABLE `users` (
 -- Zrzut danych tabeli `users`
 --
 
-INSERT INTO `users` (`login`, `password`, `permission`) VALUES
-('mietek', '1234', 1),
-('radek', '$2a$12$lgKTJ7UDEDwYzsuD9xglJOa.TFEQbrHonMDdsecOMUFrCxBXtKGAm', 2);
+INSERT INTO `users` (`user_id`, `login`, `password`, `permission`) VALUES
+(1, 'aaa', '$2a$12$sK/KFRp.CzsxyZJsjt1FQuUgy4fCiXWKVdwb0v.8Do7p56FTI0Sma', 1),
+(2, 'bbb', '$2a$12$0I3GOnC.wVLAoN1rgg6uiOiRcpG5n/MvF0aRUzc2tCoGwxMlEEXri', 1),
+(3, 'bobek', '$2a$12$b9NoA5kdRdC2OtZk9BuUeul.JqqD27hXS2FcWUTNmlM6RjXgsoj7i', 1),
+(4, 'jacek', '$2a$12$Pw6lCa4EOr93X6w43we/Guem4pIhkjARJdB.HYcY2LUat1RDC3ux6', 2),
+(5, 'mietek', '1234', 1),
+(6, 'radek', '$2a$12$lgKTJ7UDEDwYzsuD9xglJOa.TFEQbrHonMDdsecOMUFrCxBXtKGAm', 2),
+(7, 'synek', '$2a$12$QDEiWtKZ5iGI0eedwW7wde7fsLCyEP3D213vuojlGU3wmH8oUOHpC', 1),
+(8, 'trfdrbfd', '$2a$12$DbjQWljuToLcJAt1Wudf/.4U2rviFgiq75.NOa2DtDitmDXEvTjyu', 1),
+(9, 'sss', '$2a$12$MG4OTtOxOwnRPEe8YZeLB.O5/SX0bg4NbYylqZ.0kRN21sj1VvjWq', 1);
 
 --
 -- Indeksy dla zrzutów tabel
@@ -74,8 +82,9 @@ ALTER TABLE `permissions`
 -- Indeksy dla tabeli `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`login`),
-  ADD UNIQUE KEY `FK_PERMSSIONS` (`permission`);
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `USER_UNIQUE` (`login`),
+  ADD KEY `FK_PERMSSIONS` (`permission`) USING BTREE;
 
 --
 -- AUTO_INCREMENT dla zrzuconych tabel
@@ -86,6 +95,12 @@ ALTER TABLE `users`
 --
 ALTER TABLE `permissions`
   MODIFY `permission_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT dla tabeli `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Ograniczenia dla zrzutów tabel
